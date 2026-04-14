6334fe82a11ae2391bb2b105b563c56682d4a2ef
vinnikodali3 <vinnikodali3@gmail.com>
2026-04-13 22:25:10 -0400

    practice 2

---
+++ week.py
@@ -0,0 +1,9 @@
+# Get user input
+name = input("Enter your name: ")
+age = int(input("Enter your birth year: "))
+
+# Simple logic
+current_year = 2026
+calculated_age = current_year - age
+
+print(f"Hi {name}, you are approximately {calculated_age} years old!")
