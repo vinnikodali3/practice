b6caf4ff4d112cb9ed4cc139229471d0c621d57a
vinnikodali3 <vinnikodali3@gmail.com>
2026-04-13 22:21:58 -0400

    Practice

---
+++ python.py
@@ -0,0 +1,35 @@
+print("Vinuthna")
+
+print("City is Virginia")
+x = 5
+print(type(x))
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
