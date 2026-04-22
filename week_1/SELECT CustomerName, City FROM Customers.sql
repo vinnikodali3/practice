d62084e10dd9a14098494cebc7e1bd37eda1fe62
vinnikodali3 <vinnikodali3@gmail.com>
2026-04-22 17:09:18 -0400

    change

---
+++ week_1/SELECT CustomerName, City FROM Customers.sql
@@ -0,0 +1,7 @@
+SELECT CustomerName, City FROM Customers;
+SELECT * FROM Customers
+WHERE Country = 'India';
+SELECT * FROM Customers
+WHERE age<30;
+SELECT * FROM Products
+ORDER BY Price ASC;
--- week_1/python.py
+++ week_1/python.py
@@ -116,3 +116,13 @@ except:
   print("Something went wrong")
 else:
   print("Nothing went wrong")
+
+
+  Week 2
+SELECT CustomerName, City FROM Customers;
+SELECT * FROM Customers
+WHERE Country = 'India';
+SELECT * FROM Customers
+WHERE age<30;
+SELECT * FROM Products
+ORDER BY Price ASC;
